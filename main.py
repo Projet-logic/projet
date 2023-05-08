82a0fa4c89a0c7f847186be5b4d67826f46caefe
Kianfeizabadi <131669924+Kianfeizabadi@users.noreply.github.com>
2023-05-09 00:03:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def dimacs(formule, filename):
     variables = set()
 
     # Compter le nombre de clauses et ajouter chaque variable à l'ensemble
-    for clause in formule:
-        nb_clauses += 1
-        variables |= set(map(abs, clause))
+    #for clause in formule:
+     #   nb_clauses += 1
+     #   variables |= set(map(abs, clause))
 
     # Ecrire la formule au format DIMACS dans le fichier
     with open(filename, "w") as file:
@@ -35,83 +35,71 @@ def dimacs(formule, filename):
         file.write(f"p cnf {len(variables)} {nb_clauses}\n")
 
         # Ecrire chaque clause
-        for clause in formule:
-            file.write(" ".join(str(v) for v in clause) + " 0\n")
+        for clause in formule._table:
+            file.write(" ".join(str(v) for v in clause._table) + " 0\n")
 
 #---------------------------------------------------------------------------------#
-"""
-convertir la formule en une forme utilisable et solvable par pycosat
-"""
-def extract_variables(s):
-    # Diviser les chaînes en variables et calculs
-    s= s[1:len(s)-1]
 
-    # Rechercher et diviser des sous-chaînes
-    variables = re.findall(r'\((?:[^(]+|\((?:[^(]+|\([^()]+\))*\))*\)|\d+', s)
+def read_dimacs_file(filename):
+    """Reads a DIMACS format file and returns a list of clauses."""
+    clauses = []
 
-            
-    #créer des sous-listes dans une grande liste       
-    big_list = []
-    for val in variables:
-        if val[0] == "(":
-            val = val[1:len(val)-1]
-            
-        if "-" in val and "+" in val:
-            t = val.split('+')
-            big_list.append(t)
-        elif "+" in val and "-" not in val:
-            t = val.split("+")
-            big_list.append(t) 
-        else :
-            big_list.append([val])
-    #print(big_list)
-            
-    #donner des noms numériques aux variables
-    variable_names = []
-    int_variable_names = []
+    with open(filename, "r") as file:
+        for line in file:
+            if line.startswith("c") or line.startswith("p"):
+                continue  # Skip comments and problem line
+            clause = [x for x in line.split()[:-1]]  # Exclude the trailing 0
+            clauses.append(clause)
 
-    var_dict = {}
-    var_num = 1
+    return clauses
 
-    for sublist in big_list:
-        new_sublist = []
-        for item in sublist:
-            if item[0] == '-':
-                # check if the positive variable has already been assigned a name
-                pos_var = item[1:]
-                if pos_var in var_dict:
-                    new_sublist.append('-' + var_dict[pos_var])
-                else:
-                    var_dict[pos_var] = str(var_num)
-                    new_sublist.append('-' + str(var_num))
-                    var_num += 1
+def convert_variables_to_numbers(formula, variable_map):
+    """Converts variable names in the formula to numeric values based on the variable map."""
+    converted_formula = []
+
+    for clause in formula:
+        converted_clause=[]
+        for var in clause:
+            if(var[0]=='-'):
+                converted_clause.append(-variable_map[var[1:]])
             else:
-                if item in var_dict:
-                    new_sublist.append(var_dict[item])
-                else:
-                    var_dict[item] = str(var_num)
-                    new_sublist.append(str(var_num))
-                    var_num += 1
-        variable_names.append(new_sublist)
-    #print(variable_names)
-            
-    for sublist in variable_names:
-        new_sublist = []
-        for element in sublist:
-            try:
-                new_sublist.append(int(element))
-            except ValueError:
-                new_sublist.append(element)
-        int_variable_names.append(new_sublist)
-    return int_variable_names
+                converted_clause.append(variable_map[var])
+        converted_formula.append(converted_clause)
+
+    return converted_formula
+
+def convert_numbers_to_variables(result, variable_map):
+    """Converts variable names in the formula to numeric values based on the variable map."""
+    converted_result = []
+    # Create a reverse mapping of the variable map
+    variable_map_reverse = {value: key for key, value in variable_map.items()}
+    for var in result:
+        if(var<0):
+            converted_result.append('-'+variable_map_reverse[abs(var)])
+        else:
+            converted_result.append(variable_map_reverse[var])
+
+    return converted_result
+
+
+def create_num(g:Game):
+    res=dict();
+    j=1
+    for i in g.get_possib_var_horiz():
+        res[i.name]=j
+        j+=1
+    return res
+    
+
 
 #---------------------------USE PYCOSAT------------------------------------------------# 
  # Trouver une solution
-def satSolution(final_list):
+def satSolution(final_list,variable_map):
     
     solution = sat.solve(final_list)
     # Si une solution a été trouvée, l'afficher et mettre à jour le texte
     if not (solution == "UNSAT" or solution == "UNKNOWN" or solution == []):
+        solution=convert_numbers_to_variables(solution, variable_map)
         print(solution)
         print("Solution trouvée!")
     # Sinon, juste mettre a jour le texte
@@ -127,11 +115,11 @@ def main():
         print(f"formula = {f}")
         f.dev()
         print(f"development = {f}")
-        
-        final_list=extract_variables(str(f))
-        #print(final_list)
-        dimacs(final_list, "sat.cnf")
-        satSolution(final_list)       
+        variable_map=create_num(g)
+        dimacs(f, "sat.cnf")
+        final_list = read_dimacs_file("sat.cnf")
+        final_list=convert_variables_to_numbers(final_list, variable_map)
+        satSolution(final_list,variable_map)       
         
     except:
         print("you have some problem or that's end, lets restart")
