2c91ab2cb79805786cb5139210163f187df505de
Kianfeizabadi <131669924+Kianfeizabadi@users.noreply.github.com>
2023-05-09 21:57:39 +0200

    Update algo.py

--- algo.py
+++ algo.py
@@ -14,7 +14,7 @@ from copy import deepcopy
 
 def ens_possible_connexion(g:Game,c:tuple):
     
-    tmp=g.get_variable_possible()[str(c[2])]
+    tmp=g.get_variable_possible()[str(g.get_list().index(c)+1)]
     return combinations(tmp, c[2])
 
 def ex_connection(game:Game,n1,n2):
@@ -29,15 +29,15 @@ def create_formula_iterative(game:Game):
     bloc=[1]
     not_bloc=list()
     for c in game.get_list():
-        
         li=list(ens_possible_connexion(game, c))
         if(not len(li)):
             return None
         f1=formula()
+        is_first=True
         for S in li:
             list_name_in_S=list(map(lambda x:x.name,S))
-            f2=formula()
             IN=list()
+            f2=formula()
             IN_names=list()
             for var in S:
                 IN.extend(game.get_variable_interdi()[var.name[:-1]])
@@ -46,13 +46,24 @@ def create_formula_iterative(game:Game):
                     if (v.name not in list_name_in_S and v.name not in IN_names ):
                         IN.append(v)
                         IN_names.append(v.name)
-                f2*=var
+                if(is_first):
+                    f1*=var
+                else:
+                    f2=f1+var
+                    tmp=list()
                 for var_i in list(IN):
-                    f2*=(-var_i)
+                    if(is_first):
+                        f1*=-var_i
+                    else:
+                        tmp.append(f1+(-var_i))
                     IN.remove(var_i)
-            f1+=f2
+                if(not is_first):
+                    for sf in tmp:
+                        f2._table.append(sf)            
+            if(not is_first):
+                f1._table=list(f2._table)
+            is_first=False
         F*=f1
-        #F.dev()
         if (not alpha in bloc):
             added=False
             f3=formula()
@@ -74,6 +85,7 @@ def create_formula_iterative(game:Game):
                 not_bloc.append(alpha)
             F*=f3
         alpha+=1
+        F.refr()
     return F
         
         
